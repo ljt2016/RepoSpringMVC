a2a7cc36d46113cade0261048156df3566c90e78
VousAttendezrer <472080911@qq.com>
2017-10-25 15:05:58 +0800

    SpringMVC Deom

---
+++ src/main/java/com/springmvc/contriller/Hello.java
@@ -0,0 +1,15 @@
+package com.springmvc.contriller;  
+  
+  
+import org.springframework.stereotype.Controller;  
+import org.springframework.ui.Model;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+  
+@Controller  
+public class Hello {  
+    @RequestMapping(value="/Hello")  
+    public String HelloWorld(Model model){  
+        model.addAttribute("message","Hello World!!!");  
+        return "HelloWorld";  
+    }  
+}  
